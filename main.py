c7632fc9091e2e498bda75eef6d4a03041d54a3e
dwi danuaji <danuaji43@gmail.com>
2024-03-03 20:28:13 +0700

    first

---
+++ main.py
@@ -0,0 +1,11 @@
+#dwi danuaji
+
+number : int = 12
+number2 : float = 30.5
+boolean_var = True
+string_var = "my string"
+
+if __name__ == '__main__':
+    print (number + number2)
+    print (number + boolean_var)
+    #print (number + string_var)
