fd925bfff3463a9a36ba2916ef8bf06c5633a835
dwi danuaji <danuaji43@gmail.com>
2024-03-04 10:45:04 +0700

    struktur

---
+++ structure.py
@@ -0,0 +1,12 @@
+my_list = [1,2,3,4,5,'string',[1,2,4],100]
+my_map = {
+    "key_1": 1,
+    "key_2": 2
+}
+my_map_2 = {
+    "key_3": 3,
+    "key_4": 4
+}
+
+my_map.update(my_map_2)
+print(my_map)
